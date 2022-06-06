4c3fe7fb809893192c506501eb7e873804add32d
Brody Losh <brodylosh@gmail.com>
2022-06-06 11:25:10 -0400

    commit tree 9016b903ba2f54017e4ff16b758bbb79d8086751

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students    
+    end
+
+    def grades
+        students = Student.all
+        grades = students.order(:grade).reverse
+        render json: grades
+    end
+
+    def highest_grade
+        students = Student.all
+        grades = students.order(:grade).reverse
+        render json: grades.first
+    end
+end
